eb679a6a8f4395d0bcfe30c48a7859a5a1f64f95
Alexandre <alexandre.baumann11@laposte.net>
2023-02-24 13:47:09 +0100

    Initial

---
+++ FrontEnd/JS/index.js
@@ -0,0 +1,12 @@
+//http://localhost:5678/api/works
+// localhost:5678/api-docs/
+// http://127.0.0.1:5500/FrontEnd/index.html
+function main () {
+    // utiliser la méthode fetch pour récupérer les informations tu back
+    // stock les infos du back dans une variable de type tableau
+    // boucler sur le tableau
+    // dans chaque itération du tableau, créer en JS un élément html qui aura le contenu de la ligne du tableau
+    // injecter cet element html au contenu de la page
+}
+
+main ()
