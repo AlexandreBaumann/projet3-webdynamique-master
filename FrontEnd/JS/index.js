839c58a45b82ea478a487d4a57250b06f22a7c57
Alexandre <alexandre.baumann11@laposte.net>
2023-03-03 10:08:44 +0100

    Injection de la gallerie en cours

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -8,5 +8,54 @@ function main () {
     // dans chaque itération du tableau, créer en JS un élément html qui aura le contenu de la ligne du tableau
     // injecter cet element html au contenu de la page
 }
+//________________________________________________
 
-main ()
+// function main () {
+//     fetch("http://localhost:5678/api/works")
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//     .catch(error => alert("Erreur : " + error));
+// }
+
+// let donnees =( main ())
+// console.log(donnees);
+
+//________________________________________________
+
+
+// api url
+
+const api_url = "http://localhost:5678/api/works";
+
+
+function createCard (object) {
+    const gallery = document.getElementsByClassName('gallery')
+    const card = document.createElement('figure')
+    card.classname = 'card'
+    const image = document.createElement('img')
+    const figcaption = document.createElement('figcaption')
+
+    gallery.card;
+    card.append(image);
+    image.src= object.imageUrl;
+    card.append (figcaption);
+    figcaption.innerText = object.title;
+}
+
+async function getapi(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    data.forEach (element => createCard(element));
+}
+
+const donnees = [getapi(api_url)];
+
+
+// console.log (donnnees[1]) erreur "donnees is undefined"
+// console.log(donnees.length);
+
+// function extraction (object) {
+//     for (let item in object)
+//     console.log (item.title)
+// }
+// extraction (donnees)
