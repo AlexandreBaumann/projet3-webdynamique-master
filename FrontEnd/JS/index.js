7b37b7b9e71cd74e994846880d95a64049970bb3
Alexandre <alexandre.baumann11@laposte.net>
2023-03-08 15:06:32 +0100

    Affichage image

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,54 +1,63 @@
 //http://localhost:5678/api/works
 // localhost:5678/api-docs/
 // http://127.0.0.1:5500/FrontEnd/index.html
-function main () {
-    // utiliser la méthode fetch pour récupérer les informations tu back
-    // stock les infos du back dans une variable de type tableau
-    // boucler sur le tableau
-    // dans chaque itération du tableau, créer en JS un élément html qui aura le contenu de la ligne du tableau
-    // injecter cet element html au contenu de la page
-}
-//________________________________________________
 
-// function main () {
-//     fetch("http://localhost:5678/api/works")
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-//     .catch(error => alert("Erreur : " + error));
+// Bouton = gestionnaire d'événement (clic) => appel de fonction (accède à data_api)
+// => filtrage du tableau (filter)
+// Renvoyer le tableau filtré à la fonction displayData
+
+var data_Api ; 
+
+function displayData (data) {
+
+    const galleryHTML = document.getElementsByClassName('gallery')
+    data.forEach (element => {
+        const cardHTML = document.createElement('figure')
+        const imageHTML = document.createElement('img')
+        const figcaptionHTML = document.createElement('figcaption')
+        imageHTML.src = element.imageUrl
+        cardHTML.appendChild(imageHTML)
+        galleryHTML[0].appendChild(cardHTML)
+    });
+}
+  
+async function fetchData(url) {
+    const response = await fetch(url);
+    data_Api = await response.json();
+    displayData(data_Api);
+}
+
+    
+async function main  () {
+    const api_url = "http://localhost:5678/api/works";
+    await fetchData(api_url);
+}
+
+
+main()
+
+// interpolation
+
+
+
+
+
+// function createCard (object) {
+//     const gallery = document.getElementsByClassName('gallery')
+//     const card = document.createElement('figure')
+//     card.classname = 'card'
+
+
+//     gallery.card;
+//     card.append(image);
+//     image.src= object.imageUrl;
+//     card.append (figcaption);
+//     figcaption.innerText = object.title;
+//     return card;
 // }
 
-// let donnees =( main ())
-// console.log(donnees);
-
-//________________________________________________
 
 
-// api url
-
-const api_url = "http://localhost:5678/api/works";
-
-
-function createCard (object) {
-    const gallery = document.getElementsByClassName('gallery')
-    const card = document.createElement('figure')
-    card.classname = 'card'
-    const image = document.createElement('img')
-    const figcaption = document.createElement('figcaption')
-
-    gallery.card;
-    card.append(image);
-    image.src= object.imageUrl;
-    card.append (figcaption);
-    figcaption.innerText = object.title;
-}
-
-async function getapi(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    data.forEach (element => createCard(element));
-}
-
-const donnees = [getapi(api_url)];
 
 
 // console.log (donnnees[1]) erreur "donnees is undefined"
